2a85d74780c0acc8c624a738ac64ca8b5796c409
Viswanath <viswanath@cloudboxonline.com>
2012-08-03 10:10:53 +0530

    Adding Sample Test case Project

---
+++ SampleSeleniumProject/cloudbox/com/cloudmunch/selenium/AllTests.java
@@ -0,0 +1,11 @@
+package com.cloudmunch.selenium;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({SampleSeleniumTestCase.class})
+public class AllTests {
+
+}
